cd910bd06972b15558118ac3be9d88bb40a9c3f5
Bruno Vicente <brunovicente.dev@gmail.com>
2023-01-20 10:15:55 -0300

    Added button

--- Habits.js
+++ Habits.js
@@ -1,5 +1,7 @@
 const form = document.querySelector("#formHabits");
 const nlwSetup = new NLWSetup(form);
+const createDay = document.querySelector("#createDay");
+const habits = document.querySelector(".habits");
 const createHabit = document.querySelector("#createHabit");
 
 const add = () => {
@@ -18,9 +20,19 @@ const save = () => {
   localStorage.setItem("habitsDone", JSON.stringify(nlwSetup.data));
 };
 
-createHabit.addEventListener("click", add);
+createDay.addEventListener("click", add);
 
 form.addEventListener("change", save);
+
+// createHabit.addEventListener("click", () => {
+//   const dataName = prompt("What is the name of the new habit?");
+//   const emoji = prompt("What is the emoji of the new habit?");
+//   const newHabit = document.createElement("div");
+//   newHabit.classList.add("habit");
+//   newHabit.setAttribute("data-name", `${dataName}`);
+//   newHabit.innerHTML = `${emoji}`;
+//   habits.append(newHabit);
+// });
 
 const data = JSON.parse(localStorage.getItem("habitsDone")) || {};
 nlwSetup.setData(data);
